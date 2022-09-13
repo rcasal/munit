de0c2514b417dde3c1f4344a54f485fcfa7f6981
rcasal <38733103+rcasal@users.noreply.github.com>
2022-09-13 19:38:59 -0300

    Update trainer.py

--- Training/trainer.py
+++ Training/trainer.py
@@ -200,13 +200,13 @@ class MUNIT_Trainer(nn.Module):
         self.generator_a.load_state_dict(state_dict['a'])
         self.generator_b.load_state_dict(state_dict['b'])
         iterations = int(last_model_name[-11:-3])
-        del state_dict
+        torch.Cuda.empty_cache()
         # Load discriminators
         last_model_name = get_model_list(checkpoint_dir, "dis")
         state_dict = torch.load(last_model_name, map_location=map_location)
         self.dicrimininator_a.load_state_dict(state_dict['a'])
         self.discriminator_b.load_state_dict(state_dict['b'])
-        del state_dict
+        torch.Cuda.empty_cache()
         # Load optimizers
         state_dict = torch.load(os.path.join(checkpoint_dir, 'optimizer.pt'), map_location=map_location)
         self.discrimator_optimizer.load_state_dict(state_dict['dis'])
