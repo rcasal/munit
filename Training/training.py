31cf3aa388872ac6f856491f6334d26484f283b9
rcasal <38733103+rcasal@users.noreply.github.com>
2022-09-16 15:55:35 -0300

    Update training.py

--- Training/training.py
+++ Training/training.py
@@ -75,11 +75,11 @@ def train(gpu,args):
                 if rank == 0:
                     time_elapsed_training = time.time()*1000 - start_time
                     #print(f"Iteration {(iterations + 1):08d}/{args.max_iter} completed in {(time_elapsed_training // 60):.0f}m {(time_elapsed_training % 60):.0f}s {1000*time_elapsed_training % 60:.0f}ms")
-                    print(f"Iteration {(iterations + 1):08d}/{args.max_iter} completed in {(time_elapsed_training)} ms.")
+                    print(f"Iteration {(iterations + 1):08d}/{args.max_iter} completed in {(time_elapsed_training):.0f} ms.")
             else: 
                 time_elapsed_training = time.time()*1000 - start_time
                 #print(f"Iteration {(iterations + 1):08d}/{args.max_iter} completed in {(time_elapsed_training // 60):.0f}m {(time_elapsed_training % 60):.0f}s {60*time_elapsed_training % 60:.0f}ms")
-                print(f"Iteration {(iterations + 1):08d}/{args.max_iter} completed in {(time_elapsed_training)} ms.")
+                print(f"Iteration {(iterations + 1):08d}/{args.max_iter} completed in {(time_elapsed_training):.0f} ms.")
 
             start_time = time.time()*1000
 
